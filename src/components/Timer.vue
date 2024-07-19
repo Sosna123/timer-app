d4533beecc5b3ed3b190214b37e353e2e142a6c4
Sosna123 <olek.arez@gmail.com>
2024-07-19 23:15:53 +0200

    +2, dnf, - buttons work :D

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -8,11 +8,13 @@ import formatNormal from '@/js/timeFormat';
 export default defineComponent({
     setup(props, { emit }){
         //* vars
+        let jscookie = require("jscookie")
         let currentTimeStr = ref<string>("0.00")
         let currentTime = ref<number>(0)
         let startOfTimer : number|null = null
         let timerRunning: boolean = false
-        let intervalTimer:any = null
+        let intervalTimer: undefined|number
+        let timeId: number = 0
 
         //* functions
         function manageTimer(){
@@ -25,13 +27,26 @@ export default defineComponent({
                 timerRunning = true
             }else{
                 clearInterval(intervalTimer)
-                currentTime.value = new Date().getTime() - startOfTimer!
-                currentTimeStr.value = formatNormal((Math.trunc(currentTime.value/10)).toString())
+                currentTime.value = Math.trunc((new Date().getTime() - startOfTimer!) / 10)
+                currentTimeStr.value = formatNormal(currentTime.value.toString())
                 timerRunning = false
-                const time = {str: currentTimeStr.value, num: currentTime.value}
+                const time = {id: timeId, str: currentTimeStr.value, num: currentTime.value, added2: false, addedDnf: false}
+                console.log(timeId)
+                console.log(time)
+                timeId++
+                
+                jscookie.set({
+                    name:"timeId",
+                    value: timeId,
+                    exdays: 365 * 10
+                })
+
                 emit('time-done', time)
             }
+        }
 
+        if(jscookie.get("timeId")){
+            timeId = jscookie.get("timeId")
         }
 
         //* return
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="timeList w-25 bg-primary-subtle overflow-scroll p-3 d-inline-block float-start">
+    <div class="timeList bg-primary-subtle overflow-scroll p-3 d-inline-block float-start">
         <h1 class="headingTimeList">Your times:</h1>
         <ul>
             <li v-for="time in timeArray" class="fs-2">
                 {{ time.str }}
+                <button @click="modifyTime('plus2', time)">+2</button>
+                <button @click="modifyTime('dnf', time)">dnf</button>
+                <button @click="modifyTime('remove', time)">-</button>
             </li>
         </ul>
     </div>
@@ -13,13 +16,13 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Timer from "../components/Timer.vue"
-import TimeList from "../components/TimeList.vue"
+import formatNormal from '@/js/timeFormat';
 export default defineComponent({
     setup(){
         let jscookie = require("jscookie")
-        let timeArray = ref<{str:string, num:number}[]>([])
+        let timeArray = ref<{id:number, str:string, num:number, added2:boolean, addedDnf:boolean}[]>([])
         
-        function addTime(time:{str:string, num:number}){
+        function addTime(time:{id:number, str:string, num:number, added2:boolean, addedDnf:boolean}){
             timeArray.value.push(time)
             jscookie.set({
                 name:"timeList",
@@ -27,21 +30,72 @@ export default defineComponent({
                 exdays: 365 * 10
             })
         }
+
+        function modifyTime(action:"plus2"|"dnf"|"remove", time:{id:number, str:string, num:number, added2:boolean, addedDnf:boolean}){
+            if(action == "plus2"){
+                timeArray.value.forEach(e => {
+                    if(e.id == time.id && !e.added2){
+                        if(!e.addedDnf){
+                            console.log(e)
+                            e.added2 = !e.added2
+                            e.num += 200
+                            e.str = formatNormal(e.num.toString()) + "+"
+                        }
+                    }else if(e.id == time.id && e.added2){
+                        e.added2 = !e.added2
+                        e.num -= 200
+                        e.str = formatNormal(e.num.toString())
+                    }
+                })
+            }else if(action == "dnf"){
+                timeArray.value.forEach(e => {
+                    if(e.id == time.id && !e.addedDnf){
+                        if(!e.added2){
+                            e.addedDnf = !e.addedDnf
+                            e.str = `DNF(${e.str})`
+                        }
+                    }else if(e.id == time.id && e.addedDnf){
+                        e.addedDnf = !e.addedDnf
+                        e.str = formatNormal(e.num.toString())
+                    }
+                })
+            }else{
+                timeArray.value.forEach(e => {
+                    if(e.id == time.id){
+                        timeArray.value = timeArray.value.filter(e => {
+                            if(e.id != time.id){
+                                return true
+                            }
+                            return false
+                        })
+                    }
+                })
+            }
+
+            jscookie.set({
+                name:"timeList",
+                value: JSON.stringify(timeArray.value),
+                exdays: 365 * 10
+            })
+        }
         
-        timeArray = JSON.parse(jscookie.get('timeList'))
+        if(jscookie.get('timeList')){
+            timeArray.value = JSON.parse(jscookie.get('timeList'))
+        }
 
         return{
             timeArray,
-            addTime
+            addTime, modifyTime
         }
     },
-    components: {Timer, TimeList}
+    components: {Timer}
 })
 </script>
 
 <style>
 .timeList{
     height: 100vh;
+    width: 30%;
 }
 
 .headingTimeList{
