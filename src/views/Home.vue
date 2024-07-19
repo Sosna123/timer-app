70ee48be1bb367abf6cdbfef145c2700148e2a03
Sosna123 <olek.arez@gmail.com>
2024-07-19 18:20:10 +0200

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>{{ currentTimeStr }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup(){
+        let currentTimeStr = ref<string>("12.58")
+        let currentTime = ref<number>()
+
+        return{
+            currentTimeStr
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
